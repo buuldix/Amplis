d7a65ffc8a17587823181f9166044d23fc8fc97a
Mathis BOZON <Mathis.Bozon@etu.univ-smb.fr>
2022-01-04 14:31:45 +0100

    Base

---
+++ Amplis/Content/platform.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="platform" tilewidth="16" tileheight="16" tilecount="12544" columns="112">
+ <image source="_Spritesheet.png" width="1792" height="1792"/>
+</tileset>
---
+++ Amplis/Game1.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Screens;
+using MonoGame.Extended.Screens.Transitions;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using System;
+
+namespace Amplis
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Vector2 _persoPosition;
+        private int _yVelocity;
+        private int _xVelocity;
+        private int _jumpStart;
+        private bool _grounded;
+        private AnimatedSprite _perso;
+        private TiledMap _tiledMap;
+        private TiledMapTileLayer _mapLayer;
+        private TiledMapRenderer _tiledMapRenderer;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _persoPosition = new Vector2(100, 100);
+            _jumpStart = (int)_persoPosition.Y;
+            _yVelocity = 0;
+            _xVelocity = 2;
+            _grounded = true;
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            // spritesheet
+            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("motw.sf", new JsonContentLoader());
+            _perso = new AnimatedSprite(spriteSheet);
+            // TODO: use this.Content to load your game content here
+            _tiledMap = Content.Load<TiledMap>("map");
+            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
+            _mapLayer = _tiledMap.GetLayer<TiledMapTileLayer>("sol");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            float deltaSecondes = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            KeyboardState k = Keyboard.GetState();
+
+            if (_persoPosition.Y < _jumpStart)
+            {
+                _yVelocity += 1;
+            }
+            else
+            {
+                _yVelocity = 0;
+                _grounded = true;
+            }
+                
+            if (k.IsKeyDown(Keys.Space)&&_grounded)
+            {
+                _yVelocity = -10;
+                _grounded = false;
+            }
+            /*else if (k.IsKeyDown(Keys.Space))
+            {
+                if (k.IsKeyDown(Keys.D))
+                    _persoPosition.X += 10 * _xVelocity;
+                else if (k.IsKeyDown(Keys.Q))
+                    _persoPosition.X -= 10 * _xVelocity;
+            }*/
+            
+            if (k.IsKeyDown(Keys.D))
+                _persoPosition.X += _xVelocity;
+            else if (k.IsKeyDown(Keys.Q))
+                _persoPosition.X -= _xVelocity;
+
+
+            _persoPosition.Y += _yVelocity;
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _tiledMapRenderer.Draw();
+            _spriteBatch.Draw(_perso, _persoPosition);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
