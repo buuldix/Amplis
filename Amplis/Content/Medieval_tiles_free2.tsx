a8fa873e5c0c19cb79921a4a5f5513b012e2b8fa
Mathis BOZON <Mathis.Bozon@etu.univ-smb.fr>
2022-01-12 15:52:26 +0100

    medieval

--- Amplis/Content/Medieval_tiles_free2.tsx
+++ Amplis/Content/Medieval_tiles_free2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="Medieval_tiles_free2" tilewidth="8" tileheight="8" tilecount="13184" columns="128">
- <image source="C:/Users/bozonmat/source/repos/Amplis/Amplis/Content/Medieval_tiles_free2.png" width="1024" height="824"/>
+ <image source="./Medieval_tiles_free2.png" width="1024" height="824"/>
 </tileset>
