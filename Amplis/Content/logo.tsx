f339eef4afa19b34b821f20a297911268b44c690
mMmathisSs <mathis.clavioz@etu.univ-smb.fr>
2022-01-13 19:18:30 +0100

    nouvel ecran d'acceuil

--- Amplis/Content/logo.tsx
+++ Amplis/Content/logo.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="logo" tilewidth="8" tileheight="8" tilecount="8040" columns="120">
+<tileset version="1.5" tiledversion="1.7.2" name="Logo" tilewidth="16" tileheight="16" tilecount="1980" columns="60">
  <image source="./logo.png" width="960" height="540"/>
 </tileset>
