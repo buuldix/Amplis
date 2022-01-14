bcf855bc8591f549c9152376751013a1ae174851
mMmathisSs <mathis.clavioz@etu.univ-smb.fr>
2022-01-14 08:36:12 +0100

    new map

---
+++ Amplis/Content/objetM2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="objetM2" tilewidth="16" tileheight="16" margin="5" tilecount="372" columns="31">
+ <image source="Medieval_props_freeM2.png" width="512" height="200"/>
+</tileset>
