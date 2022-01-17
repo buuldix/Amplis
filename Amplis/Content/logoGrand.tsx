7d1f689a5b39201e32bd8dc8bfe8746f15ec4886
mMmathisSs <mathis.clavioz@etu.univ-smb.fr>
2022-01-17 13:31:36 +0100

    ajout d'une oueuvre d'art

---
+++ Amplis/Content/logoGrand.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="logoGrand" tilewidth="16" tileheight="16" tilecount="8040" columns="120">
+ <image source="logoGrand.png" width="1920" height="1080"/>
+</tileset>
